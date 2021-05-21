814cc6e3b4b92118a4a7b10d871bf43320597c10
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-21 23:15:29 +0530

    added:Form field validation

--- js/AddressBook.js
+++ js/AddressBook.js
@@ -1,12 +1,63 @@
 
-const fullName =document.querySelector ('#fullName');
-const textError =document.querySelector('.text-error');
-fullName.addEventListener('input',function(){
-    let fullNameRegex = RegExp('^[a-zA-Z ]+$')
-    if(fullNameRegex.test(text.value))
-    textError.textContent = "";
-    else textError.textContent = "Full Name is Incorrect";
-});
+window.addEventListener('DOMContentLoaded', (event) => {
+
+/*validating first name :
+ *Must starts with capital letter , can accept space character and must have minimum three characters
+ */
+    const fullname = document.querySelector('#fullName');
+    const textError = document.querySelector('.text-error');
+    fullname.addEventListener('input', function () {
+        if (fullname.value.length == 0) {
+            textError.textContent = "";
+            return
+        }
+        try {
+            (new Contact()).fullName = fullname.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+
+/**Validating phone number
+*Mobile no should be in 2 digit space 10 digit formate.
+*/
+
+    const phoneNo = document.querySelector('#tel');
+    const phoneError = document.querySelector('.mobile-error');
+    phoneNo.addEventListener('input', function () {
+        if (phoneNo.value.length == 0) {
+            phoneError.textContent = "";
+            return
+        }
+        try {
+            (new Contact()).phoneNumber = phoneNo.value;
+            phoneError.textContent = "";
+        } catch (e) {
+            phoneError.textContent = e;
+        }
+    });
+
+/**Validating address 
+* must have multiple words and each word with min 3 characters
+*/
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    address.addEventListener('input', function () {
+        if (address.value.length == 0) {
+            addressError.textContent = "";
+            return
+        }
+        try {
+            (new Contact()).address = address.value;
+            addressError.textContent = "";
+        } catch (e) {
+            addressError.textContent = e;
+        }
+    });
+    
+}); 
 
 
 
@@ -24,4 +75,3 @@ fullName.addEventListener('input',function(){
 
 
 
-b
